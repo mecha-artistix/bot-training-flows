92afe30b3a9d7ffb48611e04cca64b7db195605c
mecha-artistix <mecha.artistix@gmail.com>
2024-06-06 23:49:04 +0500

    add: deploy workflow

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/bot-training-flows/",
   server: {
     host: true, // This will allow Vite to listen on all IP addresses, including the host
     port: 5173, // Optional, can specify the port if you want to change it
