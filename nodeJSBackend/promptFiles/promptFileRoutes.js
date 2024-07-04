bc6566eda69b14e1545d42e5699504481c8a7d46
huzaifa <mecha.artistix@gmail.com>
2024-07-04 09:52:54 +0500

    prompt modal

--- nodeJSBackend/promptFiles/promptFileRoutes.js
+++ nodeJSBackend/promptFiles/promptFileRoutes.js
@@ -3,6 +3,6 @@ const promptFileController = require('./promptFileController');
 
 const router = express.Router();
 
-router.route('/:user').post(promptFileController.createPromptFile).get(promptFileController.getPromptFiles);
+router.route('/:user').post(promptFileController.createPromptFile);
 
 module.exports = router;
